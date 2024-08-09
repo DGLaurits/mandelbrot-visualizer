596fa39b4442a51ed4b5938af612ad5a7c6d6734
Laurits Skov Madsen <laurits.skov.madsen@gmail.com>
2024-08-09 22:08:35 +0200

    Add mandelbrot function

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ root = Tk()
 root.title("Mandelbrot")
 root.geometry("600x400")
 
+# Returns how many steps before the value escapes the circle with a radius of 2
+def mandelbrot(c, revolutions):
+
+    value = 0
+    times = 0
+
+    for _ in range(revolutions):
+        if abs(c) >= 2:
+            return times
+        value = value*value
+        times += 1
+
 root.mainloop()
