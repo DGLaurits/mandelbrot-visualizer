36a32c2601228513cf60e5978d6d46d5ed098ff7
Laurits Skov Madsen <laurits.skov.madsen@gmail.com>
2024-08-10 13:18:51 +0200

    Seperate image generation to seperate file. Use PIL for image generation

--- main.py
+++ main.py
@@ -1,51 +1,21 @@
 from tkinter import *
 from tkinter import ttk
+from mandelbrot import draw_mandelbrot
+from PIL import ImageTk, Image
 
 WIDTH = 600
 HEIGHT = 400
-REVOLUTIONS = 10
-ZOOM = 0.02
+REVOLUTIONS = 100
+ZOOM = 0.01
 
 root = Tk()
 root.title("Mandelbrot")
 
-canvas = Canvas(root, height=HEIGHT, width=WIDTH, bg = "#000000")
-canvas.pack()
-img = PhotoImage(width=WIDTH, height=HEIGHT)
-canvas.create_image((WIDTH//2, HEIGHT//2), image=img, state="normal")
+tmp_img = draw_mandelbrot((WIDTH, HEIGHT), REVOLUTIONS, ZOOM)
+img = ImageTk.PhotoImage(tmp_img)
+label = Label(image=img)
+label.image = img
+label.pack()
 
-# Returns how many steps before the value escapes the circle with a radius of 2
-def mandelbrot(c, revolutions):
-
-    c = complex(c[0], c[1])
-
-    value = 0
-    times = 0
-
-    for _ in range(revolutions):
-        if abs(value) >= 2:
-            return times
-        value = value*value + c
-        times += 1
-    return times
-
-def make_coordinates(coordinates):
-    new_x = coordinates[0]-HEIGHT/2
-    new_x = new_x * ZOOM
-    new_y = coordinates[1]-HEIGHT/2
-    new_y = new_y * ZOOM
-    return (new_x, new_y)
-
-def graph():
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            coordinates = make_coordinates((x, y))
-            mandelbrot_value = mandelbrot(coordinates, REVOLUTIONS)
-            if mandelbrot_value == REVOLUTIONS:
-                img.put("#000000", (x, y))
-            else:
-                img.put("#FFFFFF", (x, y))
-
-graph()
 
 root.mainloop()
---
+++ mandelbrot.py
@@ -0,0 +1,33 @@
+from PIL import Image, ImageColor
+
+def mandelbrot(c, revolutions):
+    c = complex(c[0], c[1])
+
+    value = 0
+    times = 0
+
+    for _ in range(revolutions):
+        if abs(value) >= 2:
+            return times
+        value = value*value + c
+        times += 1
+    return times
+
+def make_coordinates(width, height, zoom, xy):
+    new_x = xy[0]-width/2
+    new_x = new_x * zoom
+    new_y = xy[1]-height/2
+    new_y = new_y * zoom
+    return (new_x, new_y)
+
+def draw_mandelbrot(size: tuple[int, int], revolutions: int, zoom: float):
+    img = Image.new("RGB", (size[0], size[1]))
+    for x in range(size[0]):
+        for y in range(size[1]):
+            coordinates = make_coordinates(size[0], size[1], zoom, (x, y))
+            mandelbrot_value = mandelbrot(coordinates, revolutions)
+            if mandelbrot_value == revolutions:
+                img.putpixel((x, y), (200, 230, 255))
+            else:
+                img.putpixel((x, y), (0, 0, 0)) 
+    return img
