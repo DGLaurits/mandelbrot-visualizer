f34ddb8515151ae616bf9061b28871f1f8c95d74
Laurits Skov Madsen <laurits.skov.madsen@gmail.com>
2024-08-10 13:46:47 +0200

    Added zoom function

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ from tkinter import ttk
 from mandelbrot import draw_mandelbrot
 from PIL import ImageTk, Image
 
-WIDTH = 600
-HEIGHT = 400
+WIDTH = 800
+HEIGHT = 600
 REVOLUTIONS = 100
 ZOOM = 0.01
 
 root = Tk()
 root.title("Mandelbrot")
+root.resizable(False,False)
 
 tmp_img = draw_mandelbrot((WIDTH, HEIGHT), REVOLUTIONS, ZOOM)
 img = ImageTk.PhotoImage(tmp_img)
-label = Label(image=img)
-label.image = img
+label = Label(root, image=img)
 label.pack()
 
+
+def zoom_on_click(eventorigin):
+    x = eventorigin.x
+    y = eventorigin.y
+    global ZOOM
+    ZOOM *= 0.5
+    tmp_img = draw_mandelbrot((WIDTH, HEIGHT), REVOLUTIONS, ZOOM)
+    img = ImageTk.PhotoImage(tmp_img)
+    label.configure(image=img)
+    label.image = img
+
+
+label.bind("<Button 1>", zoom_on_click)
 
 root.mainloop()
